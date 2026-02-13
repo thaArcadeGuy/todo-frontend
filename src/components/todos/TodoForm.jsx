4d861beffb94400cafd8d15f6b040c09ff662457
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-02-13 16:54:50 +0300

    Add TodoForm component

---
+++ src/components/todos/TodoForm.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { todoService } from "../../services/todoService";
+import { Plus } from "lucide-react";
+
+const TodoForm =({ onTodoAdded }) => {
+  const [text, setText] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!text.trim()) return;
+
+    setSubmitting(true);
+    try {
+      const newTodo = await todoService.create(text);
+      onTodoAdded(newTodo);
+      setText("");
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <form className="flex-row" id="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        id="todo-input"
+        placeholder="Add a new task"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        autoComplete="off"
+        required 
+      />
+      <button type="submit" id="add-button" disabled={submitting}>
+        <Plus size={24} className="add-icon" />
+      </button>
+    </form>
+  );
+};
+
+export default TodoForm;
