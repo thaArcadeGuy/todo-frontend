742190a65d20ffe248f48d42bd6571e0f8af2f7d
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-02-14 00:42:12 +0300

    Separate useAuth hook from AuthContext

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,15 +1,9 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 import { authService } from "../services/authService";
 
 const AuthContext = createContext();
 
-export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error("useAuth must be used within AuthProvider")
-  }
-  return context
-}
+
 
 function AuthProviderComponent({ children }) {
   const [user, setUser] = useState(null);
