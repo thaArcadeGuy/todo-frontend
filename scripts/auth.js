a0a7817a1b61bd7030a35ec47eee0c19e6eadf5f
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-02-13 11:39:41 +0300

    Create constants

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,4 +1,5 @@
-const BASE_URL = "https://koola-mcf3.onrender.com/api";
+// const BASE_URL = "https://koola-mcf3.onrender.com/api";
+const BASE_URL = "http://localhost:8000/api"
 
 if (window.location.hash === "#login") {
   document.querySelector(".login_form").style.display = "block";
---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const BASE_URL = "https://koola-mcf3.onrender.com/api"
+
+export const TASK_STATE = {
+  ACTIVE: "active",
+  COMPLETED: "completed",
+  DELETED: "deleted"
+}
+
+export const FILTERS = {
+  ALL: "all",
+  ACTIVE: "active",
+  COMPLETED: "completed"
+}
