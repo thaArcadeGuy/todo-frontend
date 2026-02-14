d5280d0ffc87fbb33ae47232238371df1c591949
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-02-14 11:28:14 +0300

    Add TodosPage

---
+++ src/pages/TodosPage.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+import TodoList from "../components/todos/TodoList";
+import { useAuth } from "../hooks/useAuth";
+import "../styles/style.css";
+
+const TodosPage = () => {
+  const { user, loading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      navigate("/");
+    }
+  }, [user, loading, navigate]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <Header />
+      <main>
+        <TodoList />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default TodosPage;
