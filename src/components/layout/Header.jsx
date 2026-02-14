74770fca454655a6610cac231950649b11957242
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-02-14 10:33:52 +0300

    Add Header component

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+import { LogOut } from "lucide-react";
+import logoImage from "../../assets/koola-logo.png";
+
+const Header = () => {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+
+  return (
+    <header>
+      <div className="container">
+        <img src={logoImage} alt="Koola Logo" className="logo" />
+        <LogOut
+          size={24}
+          className="logout"
+          onClick={handleLogout}
+          style={{ cursor: "pointer" }} 
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
